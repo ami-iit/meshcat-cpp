23a0a6708f42c64fb4018fd0b9802d2b41f65a08
Giulio Romualdi <giulio.romualdi@gmail.com>
2023-01-03 19:14:16 +0100

    Update the simple test

--- test/meshcat_test.cpp
+++ test/meshcat_test.cpp
@@ -1,21 +1,47 @@
-#include "MeshcatCpp/Material.h"
-#include "MeshcatCpp/Shape.h"
+#include <MeshcatCpp/Material.h>
 #include <MeshcatCpp/Meshcat.h>
+#include <MeshcatCpp/Shape.h>
 
+MeshcatCpp::MatrixView<double> array_to_matrix_view(std::array<double, 16>& array)
+{
+    constexpr MeshcatCpp::MatrixView<double>::index_type rows = 4;
+    constexpr MeshcatCpp::MatrixView<double>::index_type cols = 4;
+    return MeshcatCpp::make_matrix_view(array.data(),
+                                        rows,
+                                        cols,
+                                        MeshcatCpp::MatrixView<
+                                            double>::MatrixStorageOrdering::ColumnMajor);
+}
 
-int main() {
-  MeshcatCpp::Meshcat meshcat;
-  // meshcat.set_property("/Background", "visible", false);
+int main()
+{
+    MeshcatCpp::Meshcat meshcat;
+    MeshcatCpp::Material m;
 
-  MeshcatCpp::Material m;
-  // m.wireframe = true;
-  // meshcat.set_object("box", MeshcatCpp::Box(0.1, 0.1, 0.1), m);
-  // meshcat.set_object("sphere", MeshcatCpp::Sphere(1), m);
-  meshcat.set_object("e", MeshcatCpp::Ellipsoid(1, 1.5, 0.5));
+    std::array<double, 16> transform = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
+    auto matrix_view = array_to_matrix_view(transform);
 
+    m.set_color(66, 133, 244);
+    meshcat.set_object("box", MeshcatCpp::Box(0.5, 0.5, 0.5), m);
+    matrix_view(3, 1) = 1.75;
+    meshcat.set_transform("box", matrix_view);
 
+    m.set_color(234, 67, 53);
+    meshcat.set_object("sphere", MeshcatCpp::Sphere(0.5), m);
+    matrix_view(3, 1) = 0.75;
+    meshcat.set_transform("sphere", matrix_view);
 
-  meshcat.join();
+    m.set_color(251, 188, 5);
+    meshcat.set_object("ellipsoid", MeshcatCpp::Ellipsoid(0.5, 0.25, 0.75), m);
+    matrix_view(3, 1) = -0.75;
+    meshcat.set_transform("ellipsoid", matrix_view);
 
-  return 0;
+    m.set_color(52, 168, 83);
+    meshcat.set_object("cylinder", MeshcatCpp::Cylinder(0.25, 0.5), m);
+    matrix_view(3, 1) = -1.75;
+    meshcat.set_transform("cylinder", matrix_view);
+
+    meshcat.join();
+
+    return 0;
 }
