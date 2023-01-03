d2a304ac2e9de8222d7bc33dee2aed1f1e92255d
Giulio Romualdi <giulio.romualdi@gmail.com>
2023-01-03 11:47:25 +0100

    Add a simple executable to test meshcat interface

--- test/meshcat_test.cpp
+++ test/meshcat_test.cpp
@@ -1,8 +1,19 @@
+#include "MeshcatCpp/Material.h"
+#include "MeshcatCpp/Shape.h"
 #include <MeshcatCpp/Meshcat.h>
 
 
 int main() {
   MeshcatCpp::Meshcat meshcat;
+  // meshcat.set_property("/Background", "visible", false);
+
+  MeshcatCpp::Material m;
+  // m.wireframe = true;
+  // meshcat.set_object("box", MeshcatCpp::Box(0.1, 0.1, 0.1), m);
+  // meshcat.set_object("sphere", MeshcatCpp::Sphere(1), m);
+  meshcat.set_object("e", MeshcatCpp::Ellipsoid(1, 1.5, 0.5));
+
+
 
   meshcat.join();
 
