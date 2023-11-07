4b13f5eddc481c4aee55fa17f801c5dac42ba9f6
Giulio Romualdi <giulio.romualdi@gmail.com>
2023-11-07 20:02:13 +0100

    Add the possibility to set the alpha channel in Material::set_color

--- src/Material.cpp
+++ src/Material.cpp
@@ -9,9 +9,14 @@
 
 using namespace MeshcatCpp;
 
-void Material::set_color(uint8_t r, uint8_t g, uint8_t b)
+void Material::set_color(uint8_t r, uint8_t g, uint8_t b, double a)
 {
     this->color = static_cast<int>(r << 16) + static_cast<int>(g << 8) + static_cast<int>(b);
+    this->opacity = a;
+    if (this->opacity < 1.0)
+    {
+        this->transparent = true;
+    }
 }
 
 Material Material::get_default_material()
